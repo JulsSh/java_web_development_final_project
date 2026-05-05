afc2c5d0f44292104b7608f8501c36be1ee3761f
JulsSh <j.chirokova@gmail.com>
2026-05-05 21:18:37 +0200

    added exceptions

--- eventify/ eventify-template/eventify-backend/src/main/java/ru/eventify/eventify_backend/exception/GlobalExceptionHandler.java
+++ eventify/ eventify-template/eventify-backend/src/main/java/ru/eventify/eventify_backend/exception/GlobalExceptionHandler.java
@@ -4,11 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.eventify.eventify_backend.dto.response.ErrorResponse;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,4 +26,29 @@ public class GlobalExceptionHandler {
         );
         return ResponseEntity.status(HttpStatus.CONFLICT).body(body);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(MethodArgumentNotValidException ex){
+        String fields = ex.getBindingResult().getFieldErrors().stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ErrorResponse body = new ErrorResponse(
+                "VALIDATION_FAILED",
+                fields,
+                Instant.now()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+@ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleAny(Exception ex){
+        ErrorResponse body =new ErrorResponse(
+                "INTERNAL_ERROR",
+                "An unexpected error occurred",
+               Instant.now()
+        );
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+}
+
+
 }
