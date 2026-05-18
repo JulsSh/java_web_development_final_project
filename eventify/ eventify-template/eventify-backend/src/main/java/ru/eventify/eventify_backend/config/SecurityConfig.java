7f446c690b84dd642c44db30614e6aa3f996c392
JulsSh <j.chirokova@gmail.com>
2026-05-18 13:59:07 +0200

    added Controller for booking

--- eventify/ eventify-template/eventify-backend/src/main/java/ru/eventify/eventify_backend/config/SecurityConfig.java
+++ eventify/ eventify-template/eventify-backend/src/main/java/ru/eventify/eventify_backend/config/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/bookings/**").authenticated()
                         .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.GET, "/events", "/events/**").permitAll()
